14dcfa833554da4c1e60076a4e440a2e28b97e13
Anett Seeker <noeddl@users.noreply.github.com>
2019-08-16 08:01:18 +0200

    Reorganize handling of phonetic representations

--- banone/lemma.py
+++ banone/lemma.py
@@ -1,82 +1,28 @@
 """Module providing the Lemma class."""
-import re
-
 from typing import Dict
-from typing import Iterator
-from typing import List
 from typing import Optional
+from typing import Tuple
 
-from banone.sound import Sound
+from banone.sound import SoundSequence
 
 
 class VerbStemError(Exception):
     """Custom exception to catch errors related to FastText."""
 
 
+def remove_last_syllable(phon: str) -> str:
+    """Remove the last syllable boundary in a phonetic string.
+
+    Example: Turn `fa:-n` (stem of `fa:-n@`) into `fa:n`.
+    """
+    # Find the last hyphen marking a syllable boundary and remove it.
+    i = phon.rfind("-")
+    j = i + 1
+    return phon[:i] + phon[j:]
+
+
 class Lemma:
     """A lemma as it is found in the dictionary."""
-
-    re_consonants = re.compile("[^aeiouäöü]*", re.I)
-    re_sounds = re.compile(
-        """
-          pf|t[sS]|dZ       # affricates
-        | [pbtdkg]          # plosives
-        | [fvszSZCjxh]      # fricatives
-        | [mnNlR]           # sonorants
-        | a[IU]|OY          # diphthongs
-        | [ieEaouy2][:]?    # vowels that can be lengthened (when stressed)
-        | [IOUY9]           # vowels that are always short
-        | [@6]              # schwa and vocalic r
-    """,
-        re.VERBOSE,
-    )
-    sound_char_map = {
-        "pf": ["pf"],
-        "ts": ["tz", "z"],
-        "tS": ["tsch"],
-        "dZ": ["dsch", "j"],
-        "p": ["pp", "p", "b"],
-        "b": ["bb", "b"],
-        "t": ["tt", "dt", "t", "d"],
-        "d": ["dd", "d"],
-        "k": ["ck", "kk", "k", "g"],
-        "g": ["gg", "g"],
-        "f": ["f", "v"],
-        "v": ["v", "w"],
-        "s": ["ss", "ß", "s"],
-        "z": ["s"],
-        "S": ["sch", "s"],
-        "Z": ["g"],
-        "C": ["ch"],
-        "j": ["j", "i"],
-        "x": ["ch"],
-        "h": ["h"],
-        "m": ["mm", "m"],
-        "n": ["nn", "n"],
-        "N": ["ng", "n"],
-        "l": ["ll", "l"],
-        "R": ["rr", "r"],
-        "aI": ["ai", "ei"],
-        "aU": ["au"],
-        "OY": ["äu", "eu", "oi"],
-        "i:": ["ieh", "ie", "i"],
-        "e:": ["eh", "ee", "e"],
-        "E:": ["äh", "ä"],
-        "a:": ["ah", "aa", "a"],
-        "o:": ["oh", "oo", "o"],
-        "u:": ["uh", "u"],
-        "y:": ["üh", "ü"],
-        "2:": ["öh", "ö"],
-        "I": ["i"],
-        "E": ["e", "ä"],
-        "a": ["a"],
-        "O": ["o"],
-        "U": ["u"],
-        "Y": ["ü"],
-        "9": ["ö"],
-        "@": ["e"],
-        "6": ["er", "r"],
-    }
 
     def __init__(self, orth: str = "", lemma_dict: Dict[str, str] = {}) -> None:
         """Initialize lemma from a dictionary entry."""
@@ -87,176 +33,39 @@ class Lemma:
         self.color = lemma_dict.get("color")
         self.property = lemma_dict.get("property")
         self.action = lemma_dict.get("action")
-        self.index = 0
-        self.ignore_unstressed_end = False
-        self.sounds: List[Sound] = self.map_sounds_to_characters()
-        self.syllables = self.phon.split("-")
 
-    def __len__(self) -> int:
-        """Return number of sounds of this lemma."""
-        length = len(self.sounds)
-
-        if self.ignore_unstressed_end:
-            if self.pos == "NN" and self.sounds[-1].phone == "@":
-                return length - 1
-
-            if self.pos == "VB":
-                if self.sounds[-1].phone == "n":
-                    if self.sounds[-2].phone == "@":
-                        return length - 2
-                    return length - 1
-
-        return length
-
-    def __str__(self) -> str:
-        """Return orthographic representation of this lemma."""
-        if len(self) < len(self.sounds):
-            last_sound = self.sounds[len(self)]
-            index = last_sound.start_char
-            return self.orth[:index]
-        return self.orth
-
-    def __iter__(self) -> Iterator:
-        """Return iterator."""
-        return self
-
-    def __next__(self) -> str:
-        """Return next sound of this lemma."""
-        if self.index < len(self):
-            sound = self.sounds[self.index]
-            self.index += 1
-            return sound.phone
-        raise StopIteration
-
-    def map_sounds_to_characters(self) -> List[Sound]:
-        """Create of mapping of sounds to characters."""
-        w = self.phon
-        v = self.orth.lower()
-        index = 0
-
-        sounds = []
-
-        for m in self.re_sounds.finditer(w):
-            s = m.group()
-            sound = Sound(phone=s, start_char=index)
-            for graph in self.sound_char_map.get(s, [s]):
-                if v.startswith(graph):
-                    sounds.append(sound)
-                    step = len(graph)
-                    v = v[step:]
-                    index += step
-
-        return sounds
-
-    def get_stem(self) -> str:
+    def get_stem(self) -> Tuple[str, str]:
         """Get the stem of this lemma.
 
         "Stem" for our purposes is the part of a word that can be used for
         pun compound creation.
         """
-        orth = self.orth
+        phon, orth = self.phon, self.orth
 
         # Remove Schwa sound from the end of a noun ("Fahne" becomes "Fahn").
         if self.pos == "NN":
-            if orth.endswith("e") and not orth.endswith("ie"):
-                return orth[:-1]
+            if phon.endswith("@"):
+                return remove_last_syllable(phon[:-1]), orth[:-1]
 
         # Get verb stem by removing final -en or -n.
         if self.pos == "VB":
-            if orth.endswith("en"):
-                return orth[:-2]
+            if phon.endswith("@n"):
+                return remove_last_syllable(phon[:-2]), orth[:-2]
             # "zappeln"
-            elif orth.endswith("n"):
-                return orth[:-1]
+            elif phon.endswith("n"):
+                return phon[:-1], orth[:-1]
             else:
                 msg = '"{}"" does not seem to be a verb (does not end in -en or -n)'
-                raise VerbStemError(msg.format(orth))
+                raise VerbStemError(msg.format(phon))
 
         # Default: The stem is simply the full lemma.
-        return orth
-
-    def set_start_index(self) -> None:
-        """Set the index of the iterator to the first full vowel of the word.
-
-        This is the same as ignoring the onset of the first syllable. The onset
-        of a syllable is a cluster of consonants until the first vowel.
-
-        Examples:
-        The first syllable in "Banane" is "Ba", its onset is "B".
-        The onset of "Schwan" (which consists of only one syllable) "Schw".
-        The first syllable in "Uhu" is "U", its onset is "".
-
-        """
-        for i, sound in enumerate(self.sounds):
-            if sound.is_full_vowel():
-                self.index = i
-                return
-
-        # If the word only consists of consonants (which is very unlikely)
-        # jump directly to the end.
-        self.index = len(self.sounds)
-
-    def get_distance(self, snd1: str, snd2: str) -> int:
-        """Return `True` if `snd1` and `snd2` represent two matching sounds."""
-        if snd1 == snd2:
-            return 0
-
-        if set([snd1, snd2]) == set(["a:", "a"]) or set([snd1, snd2]) == set(
-            ["o:", "o"]
-        ):
-            return 1
-
-        if (
-            set([snd1, snd2]) == set(["m", "n"])
-            or set([snd1, snd2]) == set(["l", "R"])
-            or set([snd1, snd2]) == set(["pf", "p"])
-        ):
-            return 2
-
-        return 100
-
-    def get_syllable_count(self) -> int:
-        """Return the number of syllables of the lemma."""
-        # Ignore unstressed end syllable.
-        if len(self) < len(self.sounds):
-            return len(self.syllables) - 1
-        return len(self.syllables)
+        return phon, orth
 
     def merge(self, other: "Lemma") -> Optional[str]:
         """Merge another lemma into this one to form a compound."""
-        # The base word must have more than one syllable not counting an unstressed
-        # end syllable.
-        if self.get_syllable_count() < 2:
-            return None
+        sound_seq_base = SoundSequence(self.orth, self.phon)
 
-        # Both words need to have at least the same number of syllables.
-        if other.get_syllable_count() > self.get_syllable_count():
-            return None
+        phon, orth = other.get_stem()
+        sound_seq_extra = SoundSequence(orth, phon)
 
-        # For each of the words, get the position at which the first vowel is found.
-        self.set_start_index()
-        other.set_start_index()
-
-        self.ignore_unstressed_end = False
-        other.ignore_unstressed_end = True
-
-        # Compare words.
-        for snd1 in self:
-            # Get next sound.
-            snd2 = next(other)
-
-            dist = self.get_distance(snd1, snd2)
-
-            match = dist <= 1 or (dist == 2 and len(self.syllables) > 2)
-
-            if not match:
-                return None
-
-            if other.index == len(other):
-                break
-
-        sound = self.sounds[self.index]
-        index = sound.start_char
-        s = str(other) + self.orth[index:]
-
-        return s.capitalize()
+        return sound_seq_base.merge(sound_seq_extra)
--- tests/test_lemma.py
+++ tests/test_lemma.py
@@ -1,7 +1,6 @@
 import pytest
 
 from banone.lemma import Lemma
-from banone.lemma import Sound
 from banone.lemma import VerbStemError
 from tests.utils import load_test_data
 
@@ -55,132 +54,23 @@ class TestLemma:
         assert lemma.action == lemma_dict.get("action")
 
     @pytest.mark.parametrize(
-        ("phones", "index"),
+        ("orth", "lemma_dict", "phon_stem", "orth_stem"),
         [
-            (["a", "n", "a", "n", "a", "s"], 0),
-            (["b", "a", "n", "a:", "n", "@"], 1),
-            (["b", "l", "aU"], 2),
-            (["b"], 1),
+            ("Haus", {"phon": "haUs", "pos": "NN"}, "haUs", "Haus"),
+            ("Magie", {"phon": "ma-gi:", "pos": "NN"}, "ma-gi:", "Magie"),
+            ("Fahne", {"phon": "fa:-n@", "pos": "NN"}, "fa:n", "Fahn"),
+            ("Banane", {"phon": "ba-na:-n@", "pos": "NN"}, "ba-na:n", "Banan"),
+            ("bauen", {"phon": "baU-@n", "pos": "VB"}, "baU", "bau"),
+            ("malen", {"phon": "ma:-l@n", "pos": "VB"}, "ma:l", "mal"),
+            ("anmalen", {"phon": "an-ma:-l@n", "pos": "VB"}, "an-ma:l", "anmal"),
+            ("zappeln", {"phon": "tsa-p@ln", "pos": "VB"}, "tsa-p@l", "zappel"),
+            ("toll", {"phon": "tOl", "pos": "ADJ"}, "tOl", "toll"),
         ],
     )
-    def test_set_start_index(self, phones, index):
-        lemma = Lemma()
-        lemma.sounds = [
-            Sound(phone=s, start_char=i)
-            for (s, i) in zip(phones, range(0, len(phones)))
-        ]
-        lemma.set_start_index()
-
-        assert lemma.index == index
-
-    @pytest.mark.parametrize(
-        ("phones", "index", "sound"),
-        [
-            (["a", "n", "a", "n", "a", "s"], 0, "a"),
-            (["b", "a", "n", "a:", "n", "@"], 3, "a:"),
-            (["b", "l", "aU"], 2, "aU"),
-        ],
-    )
-    def test_next(self, phones, index, sound):
-        lemma = Lemma()
-        lemma.sounds = [
-            Sound(phone=s, start_char=i)
-            for (s, i) in zip(phones, range(0, len(phones)))
-        ]
-        lemma.index = index
-
-        assert next(lemma) == sound
-
-    @pytest.mark.parametrize(("phones", "index"), [(["a", "n", "a", "n", "a", "s"], 6)])
-    def test_next_at_end_of_list(self, phones, index):
-        lemma = Lemma()
-        lemma.sounds = [
-            Sound(phone=s, start_char=i)
-            for (s, i) in zip(phones, range(0, len(phones)))
-        ]
-        lemma.index = index
-
-        with pytest.raises(StopIteration):
-            next(lemma)
-
-    @pytest.mark.parametrize(
-        ("orth", "phon", "phones", "phones2graphs"),
-        [
-            ("Pfahl", "pfa:l", ["pf", "a:", "l"], [0, 2, 4]),
-            ("Zahl", "tsa:l", ["ts", "a:", "l"], [0, 1, 3]),
-            ("deutsch", "dOYtS", ["d", "OY", "tS"], [0, 1, 3]),
-            ("Dschungel", "dZUN@l", ["dZ", "U", "N", "@", "l"], [0, 4, 5, 7, 8]),
-            ("Pein", "paIn", ["p", "aI", "n"], [0, 1, 3]),
-            ("Bein", "baIn", ["b", "aI", "n"], [0, 1, 3]),
-            ("Teich", "taIC", ["t", "aI", "C"], [0, 1, 3]),
-            ("Deich", "daIC", ["d", "aI", "C"], [0, 1, 3]),
-            ("Kunst", "kUnst", ["k", "U", "n", "s", "t"], [0, 1, 2, 3, 4]),
-            ("Gunst", "gUnst", ["g", "U", "n", "s", "t"], [0, 1, 2, 3, 4]),
-            ("fast", "fast", ["f", "a", "s", "t"], [0, 1, 2, 3]),
-            ("was", "vas", ["v", "a", "s"], [0, 1, 2]),
-            ("Tasse", "tas@", ["t", "a", "s", "@"], [0, 1, 2, 4]),
-            ("Hase", "ha:z@", ["h", "a:", "z", "@"], [0, 1, 2, 3]),
-            ("waschen", "vaS@n", ["v", "a", "S", "@", "n"], [0, 1, 2, 5, 6]),
-            ("Genie", "Zeni:", ["Z", "e", "n", "i:"], [0, 1, 2, 3]),
-            ("sicher", "zIC6", ["z", "I", "C", "6"], [0, 1, 2, 4]),
-            ("Jahr", "ja:6", ["j", "a:", "6"], [0, 1, 3]),
-            ("Buch", "bu:x", ["b", "u:", "x"], [0, 1, 2]),
-            ("Hand", "hant", ["h", "a", "n", "t"], [0, 1, 2, 3]),
-            ("mein", "maIn", ["m", "aI", "n"], [0, 1, 3]),
-            ("nein", "naIn", ["n", "aI", "n"], [0, 1, 3]),
-            ("Ding", "dIN", ["d", "I", "N"], [0, 1, 2]),
-            ("Leim", "laIm", ["l", "aI", "m"], [0, 1, 3]),
-            ("Reim", "RaIm", ["R", "aI", "m"], [0, 1, 3]),
-            ("Eis", "aIs", ["aI", "s"], [0, 2]),
-            ("Haus", "haUs", ["h", "aU", "s"], [0, 1, 3]),
-            ("Kreuz", "kROYts", ["k", "R", "OY", "ts"], [0, 1, 2, 4]),
-            ("Lied", "li:t", ["l", "i:", "t"], [0, 1, 3]),
-            ("Beet", "be:t", ["b", "e:", "t"], [0, 1, 3]),
-            ("spät", "SpE:t", ["S", "p", "E:", "t"], [0, 1, 2, 3]),
-            ("Tat", "ta:t", ["t", "a:", "t"], [0, 1, 2]),
-            ("rot", "Ro:t", ["R", "o:", "t"], [0, 1, 2]),
-            ("Blut", "blu:t", ["b", "l", "u:", "t"], [0, 1, 2, 3]),
-            ("süß", "zy:s", ["z", "y:", "s"], [0, 1, 2]),
-            ("blöd", "bl2:t", ["b", "l", "2:", "t"], [0, 1, 2, 3]),
-            ("Sitz", "zIts", ["z", "I", "ts"], [0, 1, 2]),
-            ("Gesetz", "g@zEts", ["g", "@", "z", "E", "ts"], [0, 1, 2, 3, 4]),
-            ("Satz", "zats", ["z", "a", "ts"], [0, 1, 2]),
-            ("Trotz", "tROts", ["t", "R", "O", "ts"], [0, 1, 2, 3]),
-            ("Schutz", "SUts", ["S", "U", "ts"], [0, 3, 4]),
-            ("hübsch", "hYpS", ["h", "Y", "p", "S"], [0, 1, 2, 3]),
-            (
-                "plötzlich",
-                "pl9tslIC",
-                ["p", "l", "9", "ts", "l", "I", "C"],
-                [0, 1, 2, 3, 5, 6, 7],
-            ),
-            ("bitte", "bIt@", ["b", "I", "t", "@"], [0, 1, 2, 4]),
-            ("bitter", "bIt6", ["b", "I", "t", "6"], [0, 1, 2, 4]),
-        ],
-    )
-    def test_map_sounds_to_characters(self, orth, phon, phones, phones2graphs):
-        lemma = Lemma(orth, {"phon": phon})
-        # map_sounds_to_characters is called on initialization.
-
-        sounds = [Sound(phone=s, start_char=i) for (s, i) in zip(phones, phones2graphs)]
-
-        assert lemma.sounds == sounds
-
-    @pytest.mark.parametrize(
-        ("orth", "lemma_dict", "stem"),
-        [
-            ("Haus", {"pos": "NN"}, "Haus"),
-            ("Fahne", {"pos": "NN"}, "Fahn"),
-            ("Magie", {"pos": "NN"}, "Magie"),
-            ("bauen", {"pos": "VB"}, "bau"),
-            ("zappeln", {"pos": "VB"}, "zappel"),
-            ("toll", {"pos": "ADJ"}, "toll"),
-        ],
-    )
-    def test_get_stem(self, orth, lemma_dict, stem):
+    def test_get_stem(self, orth, lemma_dict, phon_stem, orth_stem):
         lemma = Lemma(orth, lemma_dict)
 
-        assert lemma.get_stem() == stem
+        assert lemma.get_stem() == (phon_stem, orth_stem)
 
     @pytest.mark.parametrize(("orth", "lemma_dict"), [("keinverb", {"pos": "VB"})])
     def test_get_verb_stem_with_error(self, orth, lemma_dict):
@@ -212,6 +102,8 @@ class TestLemma:
             ("Fahne", "spannen"),
             # The base word should not have less syllables than the extra word.
             ("Pudel", "Nudelauflauf"),
+            # If two stressed vowels shall be matched they must have the same length.
+            ("Tannzapfen", "Fahne"),
         ],
     )
     def test_merge_fail(self, str1, str2, fxt_dict):
